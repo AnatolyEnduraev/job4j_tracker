eb6ec953d96eff86b3730319ebe7858c5e387ce9
Анатолий <eao73@mail.ru>
2025-04-27 00:46:00 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,20 @@
 package ru.job4j.tracker;
 
-public class Item {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class Item {
+    private LocalDateTime created;
+    LocalDateTime currentDateTime = LocalDateTime.now();
     private int id;
     private String name;
 
     public Item() {
+        this.created = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public Item(String name) {
@@ -31,5 +40,15 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+}
+
+class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime createdDate = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String formattedDate = createdDate.format(formatter);
+        System.out.println("Дата создания: " + formattedDate);
     }
 }
