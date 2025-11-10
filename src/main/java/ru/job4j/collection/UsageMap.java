d99febf05e088d7e6a4ac6d5bc52bdc91dd5f8c6
Анатолий <eao73@mail.ru>
2025-11-10 20:30:47 +0300

    0. Map, HashMap [#212652 #554603]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("eao73@mail.ru", "Anatoly Enduraev");
+        map.put("eao73@mail.ru", "Anatoly Enduraev Olegovich");
+        map.put("dmz73@mail.ru", "Ekaterina Orlova");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
 
         for (String key : map.keySet()) {
             String value = map.get(key);
