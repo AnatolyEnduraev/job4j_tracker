f07c45ee45ec8e02aaaacc20b6d2b9b0ab381c17
Анатолий <eao73@mail.ru>
2025-11-14 20:15:09 +0300

    1. Паспорт и жители [#212656 #554662]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,4 +11,14 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenTestAddDublicatePassport() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Anatoly Enduraev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        boolean result = office.add(citizen2);
+        assertThat(result).isFalse();
+    }
 }
